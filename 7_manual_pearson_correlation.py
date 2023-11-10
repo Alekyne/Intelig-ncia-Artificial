7a7f19eadd4e5873244966488e8543e68b0f337b
Alekyne Ribeiro <71844499+Alekyne@users.noreply.github.com>
2023-11-10 20:05:21 -0300

    Add files via upload

---
+++ 7_manual_pearson_correlation.py
@@ -0,0 +1,27 @@
+# Perform the necessary imports
+import matplotlib.pyplot as plt
+import numpy as np
+
+from src.utils import load_grains_dataset
+
+
+
+def pearson_correlation(x,y):
+    r = sum((x - np.mean(x)) * (y - np.mean(y))) / np.sqrt(sum((x - np.mean(x)) ** 2) * sum(((y - np.mean(y))**2)))
+    return r
+
+
+grains_df = load_grains_dataset()
+
+
+# Assign the 0th column of grains: width
+width = grains_df['0']
+
+# Assign the 1st column of grains: length
+length = grains_df['1']
+
+# Calculate the Pearson correlation
+correlation = pearson_correlation(width,length)
+
+# Display the correlation
+print(correlation)
